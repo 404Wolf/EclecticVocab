d00861e8b0f9959f99d4fc941fa7b82c0703ff0e
Wolf <caffeinate@msn.com>
2023-04-23 18:39:26 -0400

    Improve functionality

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from pprint import pprint
-
 from dotenv import load_dotenv
 from os import getenv
 import openai
 import json
 load_dotenv()
+
+input_word = input("Enter a word: ")
 
 openai.api_key = getenv("TOKEN")
 with open("system.txt") as systemFile:
@@ -14,11 +14,15 @@ output = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
         {"role": "system", "content": system_description},
-        {"role": "user", "content": "careen"},
+        {"role": "user", "content": input_word},
     ],
-)
+)["choices"][0]["message"]["content"]
 
-print(output)
-output = output["choices"][0]["message"]["content"]
-with open("output.json", "w") as outputFile:
-    json.dump(json.loads(output), outputFile, indent=4)
+try:
+    output = json.loads(output)
+except json.decoder.JSONDecodeError:
+    print("Error: Output is not valid JSON")
+    print(output)
+
+with open(f"outputs/{output['word'].lower()}.json", "w") as outputFile:
+    json.dump(output, outputFile, indent=4)
